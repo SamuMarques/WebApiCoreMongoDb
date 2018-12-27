c114c9f36a2c8027e43c1651089833245be74583
SamuMarques <samuelmarquescruz@hotmail.com>
2018-12-27 00:29:02 -0200

    Comit Final

---
+++ WebApiCoreWithMongoDb/WebApiCoreWithMongoDb/Context.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCoreWithMongoDb.Models;
+
+namespace WebApiCoreWithMongoDb
+{
+    public class Context
+    {
+        private readonly IMongoDatabase database;
+
+        public Context()
+        {
+            database = new MongoClient("mongodb://admin:desenv1@ds113873.mlab.com:13873/samuelsdb").GetDatabase("samuelsdb");
+        }
+
+        public IMongoCollection<Post> Posts
+        {
+            get
+            {
+                return database.GetCollection<Post>("Posts");
+            }
+        }
+    }
+}
---
+++ WebApiCoreWithMongoDb/WebApiCoreWithMongoDb/Controllers/PostController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApiCoreWithMongoDb.Models;
+
+namespace WebApiCoreWithMongoDb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private Context context;
+
+        public PostController()
+        {
+            context = new Context();
+        }
+        // GET: api/Post
+        [HttpGet]
+        public IEnumerable<Post> Get()
+        {
+            var posts = context.Posts.Find(x => true).ToList();
+            return posts;
+        }
+
+        // GET: api/Post/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Post
+        [HttpPost]
+        public IActionResult Post([FromBody]Post value)
+        {
+            try
+            {
+                context.Posts.InsertOne(value);
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+                
+            }
+        }
+
+        // PUT: api/Post/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
